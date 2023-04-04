10ae315d5947338644606482f2178a12ac778fd9
Виктор Горбунцов <gorbuntsov_v@mail.ru>
2023-04-04 19:16:25 +0300

    исправлены замечания pep8

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -37,9 +37,9 @@ class Group(models.Model):
     slug = models.SlugField(unique=True, verbose_name='Параметр')
     description = models.TextField(verbose_name='Описание')
 
-    #class Meta:
-    #    verbose_name = 'Заголовок'
-    #    verbose_name_plural = 'Заголовки'
+    class Meta:
+        verbose_name = 'Заголовок'
+        verbose_name_plural = 'Заголовки'
 
     def __str__(self):
         return self.title
