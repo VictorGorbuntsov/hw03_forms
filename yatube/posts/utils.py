195baecbfca289d7e078a1c4df2c1dbc1fc29d48
Виктор Горбунцов <gorbuntsov_v@mail.ru>
2023-04-21 15:39:49 +0300

    !

--- yatube/posts/utils.py
+++ yatube/posts/utils.py
@@ -1,9 +1,8 @@
-from django.conf import settings
 from django.core.paginator import Paginator
 
 
 def get_page(request, post_list):
-    paginator = Paginator(post_list, settings.POSTS_ON_PAGE)
+    paginator = Paginator(post_list, 10)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     return page_obj
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -16,7 +16,7 @@ def index(request):
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = group.posts.all()[:settings.POSTS_ON_PAGE]
+    posts = group.posts.all()
     context = {
         'group': group,
         'posts': posts,
